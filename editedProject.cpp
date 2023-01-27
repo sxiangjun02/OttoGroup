b88804a5892a7e0c461fc76093d1ccb660c648d7
sxiangjun02 <epic002xd@gmail.com>
2023-01-28 01:18:38 +0800

    otto

--- editedProject.cpp
+++ editedProject.cpp
@@ -5,26 +5,38 @@
 
 using namespace std;
 int displayTitle();
-void carRental();
-int membership();
+int carRental(bool member);
+bool membership();
 void signUp();
+bool logIn();
 int main()
 {
-	int page, i;
-	page = displayTitle();
-	system("cls");
-	if (page == 1)
+	int page = 0;
+	bool i; // membersip yes or no
+	while (page != 4) // allow people to return to main menu
 	{
-		carRental();
+		system("cls");
+		page = displayTitle();
+		system("cls");
+		if (page == 1) // rent car
+		{
+			page = carRental(i);
 
+		}
+		else if (page == 2) // return car
+		{
+			/* request user to pay off remaining rental fee
+			can try to request user the mileage on the car.
+			Then charge the user fixed rate on extra mileage.
+			But if want to do this, then the txt file must update to include limited or unlimited mileage.
+			Just suggesting. */
+		}
+		else if (page == 3) // apply membership
+		{
+			i = membership();
+		}
+		else cout << "Thank you for visiting! Have a nice day." << endl;
 	}
-	else if (page == 2)
-	{ }
-	else if (page == 3)
-	{
-		i = membership();
-	}
-	else cout << "Thank you for visiting! Have a nice day." << endl;
 
 	system("pause");
 	return 0;
@@ -43,10 +55,10 @@ int displayTitle()
 	if (choice == 1) return 1;
 	else if (choice == 2) return 2;
 	else if (choice == 3) return 3;
-	else return 0;
+	else return 4;
 }
 
-void carRental()
+int carRental(bool member)
 {
 	const int SIZE = 4, pSize = 4; // change value according to number of model
 	ifstream inFile;
@@ -82,7 +94,13 @@ void carRental()
 			if (model[i] == rentModel)
 			{
 				system("cls");
-				cout << "\n" << model[i] << endl;
+				if (member == true) // applying member discount (taken for membership())
+				{
+					price[i] = price[i] / 100.0 * 90;
+					cout << "Member's 10% discount applied." << endl;
+				}
+				cout << "\nModel : " << model[i] << endl;
+				
 				cout << "Rental Price is $" << price[i] << endl;
 				deposit_display = price[i] / 100.0 * 60;
 				break;
@@ -106,22 +124,37 @@ void carRental()
 		cout << "\nTrasaction Approve!" << endl;
 		Sleep(1000);
 		system("cls");
-		cout << "\nEnjoy your trip!" << endl;
+		cout << "Enjoy your trip!" << endl;
 	}
+	return 4;
+
 }
 
-int membership()
+// membership functions
+bool membership()
 {
 	int page;
+	bool member = false;
 	cout << "Welcome to the Membership store" << endl;
 	cout << "Members get a 10% discount on all rentals." << endl;
 	cout << "\n1. Sign Up" << endl;
 	cout << "2. Log In" << endl;
-	cout << "Signing up, or already have an account : " << endl;
+	cout << "Enter any other to return to menu." << endl;
+	cout << "Signing up, or already have an account : ";
 	cin >> page;
-	if (page == 1) signUp();
-
-	return 0;
+	if (page == 1)
+	{
+		signUp();
+		member = true;
+	}
+	else if (page == 2)
+	{
+		member = logIn();
+	}
+		cout << "\n Returning to main menu..." << endl;
+		Sleep(1000);
+	
+	return member;
 }
 
 void signUp()
@@ -129,14 +162,13 @@ void signUp()
 	ofstream outFile;
 	string name;
 	int payment;
-	cout << "Membership fee is $50/month" << endl;
+	cout << "\nMembership fee is $50/month" << endl;
 	cout << "Please enter username: ";
 	cin >> name;
 	cout << "Please make payment of $50 : ";
 	cin >> payment;
 	while (payment != 50)
 	{
-		//Sleep(1000);
 		cout << "\nProcessing..." << endl;
 		Sleep(1000);
 		cout << "\nTransaction Disapprove" << endl;
@@ -147,7 +179,7 @@ void signUp()
 	Sleep(1000);
 	cout << "\nTrasaction Approve!" << endl;
 	Sleep(1000);
-	outFile.open("membership.txt", ios::app);
+	outFile.open("membership.txt", ios::app); // allow data to be save into text file
 	if (!outFile) cout << "Error" << endl;
 	else
 	{
@@ -160,7 +192,41 @@ void signUp()
 	outFile.close();
 }
 
-void logIn()
+bool logIn() // determine if user is a member or not
 {
+	ifstream inFile;
+	string name, username;
+	bool member;
+	cout << "Enter username: ";
+	cin >> username;
+	
+	while (username != name)
+	{
+		inFile.open("membership.txt");
+		if (!inFile) cout << "Error! Unable to open file" << endl;
+		while (!inFile.eof()) // read txt file for user name
+		{
+			if (inFile.fail())
+			{
+				cout << "Error fail" << endl;
+				break;
+			}
+			inFile >> name;
+			
+			if (username == name) break;
+		}
+		inFile.close();
+		cout << "\nLogging in..." << endl;
+		Sleep(1000);
+		if (username != name)
+		{
+			cout << "Invalid username. Please try again: ";
+			cin >> username;
+		}
 
+	}
+	cout << "Welcome back " << username << "!" << endl;
+	member = true;
+	
+	return member;
 }
