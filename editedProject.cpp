0db4004856623e7ee109b0a4dd9db83f14ce6c9e
sxiangjun02 <epic002xd@gmail.com>
2023-02-03 15:41:28 +0800

    update

--- editedProject.cpp
+++ editedProject.cpp
@@ -9,10 +9,11 @@ int carRental(bool member);
 bool membership();
 void signUp();
 bool logIn();
+void carReturn();
 int main()
 {
 	int page = 0;
-	bool i; // membersip yes or no
+	bool i = false; // membersip yes or no
 	while (page != 4) // allow people to return to main menu
 	{
 		system("cls");
@@ -25,14 +26,9 @@ int main()
 		}
 		else if (page == 2) // return car
 		{
-			/* request user to pay off remaining rental fee
-			can try to request user the mileage on the car.
-			Then charge the user fixed rate on extra mileage.
-			But if want to do this, then the txt file must update to include limited or unlimited mileage.
-			Just suggesting. 
-			
-			aLso if really want to do this, u need to create another txt file, but i can guide u thru it.
-			*/
+			carReturn();
+			cout << "Thank you for renting! Have a nice day!" << endl;
+			break;
 		}
 		else if (page == 3) // apply membership
 		{
@@ -64,70 +60,82 @@ int displayTitle()
 int carRental(bool member)
 {
 	const int SIZE = 4, pSize = 4; // change value according to number of model
-	ifstream inFile;
-	string model[SIZE], rentModel;
-	int quantity;
+	ifstream carList;
+	ofstream rental; // to save customer who rents
+	string model[SIZE], rentModel, name;
+	int quantity, memberRent = 0;
 	double price[pSize], deposit, deposit_display;
-	inFile.open("Project2.txt");
-	if (!inFile) cout << "Error! Unable to open file" << endl;
+	carList.open("Project2.txt");
+	if (!carList) cout << "Error! Unable to open file" << endl;
 	else
 	{
 		// display available car
 		cout << "Available rental cars." << endl;
 		cout << left << setw(15) << "Model" << setw(15) << "Quantity" << "Price($)" << endl;
-		while (!inFile.eof())
+		while (!carList.eof())
 		{
-			if (inFile.fail())
+			if (carList.fail())
 			{
 				cout << "Error!" << endl;
 				break;
 			}
 			for (int i = 0; i < SIZE; i++)
 			{
-				inFile >> model[i] >> quantity >> price[i];
+				carList >> model[i] >> quantity >> price[i];
 				cout << setw(15) << model[i] << setw(15) << quantity << price[i] << endl;
 			}
 		}
-		inFile.close();
+		carList.close();
 		// applying rental
-		cout << "Please enter model name to rent: ";
-		cin >> rentModel;
-		for (int i = 0; i < SIZE; i++)
+		rental.open("Rental.txt", ios::app);
+		if (!rental) cout << "Error" << endl;
+		else
 		{
-			if (model[i] == rentModel)
+			cout << "Please enter model name to rent: ";
+			cin >> rentModel;
+			for (int i = 0; i < SIZE; i++)
 			{
-				system("cls");
-				if (member == true) // applying member discount (taken for membership())
+				if (model[i] == rentModel)
 				{
-					price[i] = price[i] / 100.0 * 90;
-					cout << "Member's 10% discount applied." << endl;
+					system("cls");
+					if (member == true) // applying member discount (taken for membership())
+					{
+						price[i] = price[i] / 100.0 * 90;
+						cout << "Member's 10% discount applied." << endl;
+						memberRent = 1;
+					}
+					cout << "\nModel : " << model[i] << endl;
+
+					cout << "Rental Price is $" << price[i] << endl;
+					deposit_display = price[i] / 100.0 * 60;
+					break;
 				}
-				cout << "\nModel : " << model[i] << endl;
-				
-				cout << "Rental Price is $" << price[i] << endl;
-				deposit_display = price[i] / 100.0 * 60;
-				break;
 			}
-		}
-		// paying deposit
-		cout << "Minimum deposit amount is $" << deposit_display << endl;
-		cout << "Please enter amount to deposit to complete the rental process : ";
-		cin >> deposit;
-		while (deposit < deposit_display)
-		{
-			//Sleep(1000);
+			// paying deposit
+			cout << "Minimum deposit amount is $" << deposit_display << endl;
+			cout << "Please enter amount to deposit to complete the rental process : ";
+			cin >> deposit;
+			while (deposit < deposit_display)
+			{
+				//Sleep(1000);
+				cout << "\nProcessing..." << endl;
+				Sleep(1000);
+				cout << "\nTransaction Disapprove" << endl;
+				cout << "Please try again : " << endl;
+				cin >> deposit;
+			}
 			cout << "\nProcessing..." << endl;
 			Sleep(1000);
-			cout << "\nTransaction Disapprove" << endl;
-			cout << "Please try again : " << endl;
-			cin >> deposit;
+			cout << "\nTrasaction Approve!" << endl;
+			Sleep(1000);
+			cout << "Please enter name: ";
+			cin >> name;
+			system("cls");
+			cout << "Thank you." << endl;
+			cout << "Enjoy your trip!" << endl;
+			rental << name << "\t" << deposit << "\t" << rentModel << "\t" << memberRent << endl;
+			rental.close();
 		}
-		cout << "\nProcessing..." << endl;
-		Sleep(1000);
-		cout << "\nTrasaction Approve!" << endl;
-		Sleep(1000);
-		system("cls");
-		cout << "Enjoy your trip!" << endl;
 	}
 	return 4;
 
@@ -163,11 +171,13 @@ bool membership()
 void signUp()
 {
 	ofstream outFile;
-	string name;
+	string name, password;
 	int payment;
 	cout << "\nMembership fee is $50/month" << endl;
 	cout << "Please enter username: ";
 	cin >> name;
+	cout << "please enter password: ";
+	cin >> password;
 	cout << "Please make payment of $50 : ";
 	cin >> payment;
 	while (payment != 50)
@@ -186,11 +196,10 @@ void signUp()
 	if (!outFile) cout << "Error" << endl;
 	else
 	{
-		outFile << name << endl;
+		outFile << name << "\t" << password << endl;
 		system("cls");
 		cout << "member's account created" << endl;
 		Sleep(1000);
-		cout << "\nReturning to main menu." << endl;
 	}
 	outFile.close();
 }
@@ -198,12 +207,14 @@ void signUp()
 bool logIn() // determine if user is a member or not
 {
 	ifstream inFile;
-	string name, username;
+	string name, username, password, checkPass;
 	bool member;
-	cout << "Enter username: ";
+	cout << "\nEnter username: ";
 	cin >> username;
+	cout << "Enter password: ";
+	cin >> password;
 	
-	while (username != name)
+	while (username != name || password != checkPass)
 	{
 		inFile.open("membership.txt");
 		if (!inFile) cout << "Error! Unable to open file" << endl;
@@ -214,22 +225,89 @@ bool logIn() // determine if user is a member or not
 				cout << "Error fail" << endl;
 				break;
 			}
-			inFile >> name;
+			inFile >> name >> checkPass;
 			
-			if (username == name) break;
+			if (name == username && checkPass == password) break;
 		}
 		inFile.close();
 		cout << "\nLogging in..." << endl;
 		Sleep(1000);
-		if (username != name)
+		if (username != name || checkPass != password)
 		{
-			cout << "Invalid username. Please try again: ";
+			system("cls");
+			cout << "Invalid username/password. Please try again" << endl;
+			cout << "Enter username: ";
 			cin >> username;
+			cout << "Enter password: ";
+			cin >> password;
 		}
-
 	}
 	cout << "Welcome back " << username << "!" << endl;
 	member = true;
 	
 	return member;
+}
+
+// returning the car
+void carReturn()
+{
+	ifstream carList, rental;
+	string name, checkName, model, checkModel;
+	int deposit, member, balance, quantity, price, payment;
+
+	rental.open("Rental.txt");
+	carList.open("Project2.txt");
+	if (!rental || !carList) cout << "Error" << endl;
+	else
+	{
+		cout << "Please enter rental's name: ";
+		cin >> name;
+		while (!rental.eof())
+		{
+			if (rental.fail())
+			{
+				cout << "Error" << endl;
+				break;
+			}
+			rental >> checkName >> deposit >> model >> member;
+			if (checkName == name) break;
+		}
+		rental.close();
+		while (!carList.eof())
+		{
+			if (carList.fail())
+			{
+				cout << "Error" << endl;
+				break;
+			}
+			carList >> checkModel >> quantity >> price;
+			if (checkModel == model) break;
+		}
+		if (member == 1) // make sure that the correct amount is paid
+		{
+			price = price / 100 * 90;
+		}
+		balance = price - deposit;
+		cout << "Rental's name: " << name << endl;
+		cout << "Model rented: " << model << endl;
+		cout << "Rent amount: " << price << endl;
+		cout << "Deposit amount: " << deposit << endl;
+		cout << "Balance : " << balance << endl;
+		cout << "\n Please pay remaining balances : ";
+		cin >> payment;
+
+		while (payment != balance)
+		{
+			cout << "\nProcessing..." << endl;
+			Sleep(1000);
+			cout << "\nTransaction Disapprove" << endl;
+			cout << "Please try again : " << endl;
+			cin >> payment;
+		}
+		cout << "\nProcessing..." << endl;
+		Sleep(1000);
+		cout << "\nTrasaction Approve!" << endl;
+		Sleep(1000);
+		system("cls");
+	}
 }
